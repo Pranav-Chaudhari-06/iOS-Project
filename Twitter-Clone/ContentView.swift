4b6067d7777fa741cd54b33dafbd8324551f0c88
Pranav Chaudhari <21bmiit096@gmail.com>
2025-03-17 10:14:01 +0530

    Initial Commit

---
+++ Twitter-Clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Twitter-Clone
+//
+//  Created by BMIIT on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by BMIIT on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
